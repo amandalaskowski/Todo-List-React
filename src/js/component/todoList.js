97caa90779925be003a0c2e05886e74d2897c99c
amandalaskowski <amanda.2002@aol.com>
2021-07-29 03:06:13 +0000

    almost done

--- src/js/component/todoList.js
+++ src/js/component/todoList.js
@@ -27,28 +27,31 @@ export function TodoList() {
 	return (
 		<div className="body">
 			<h1>todos</h1>
-			<input
-				type="text"
-				id="first_name"
-				value={userInput}
-				onKeyDown={handleKeyPress}
-				onChange={event => {
-					setUserInput(event.target.value);
-				}}></input>
-			<ul>
-				{todolist.map((task, i) => {
-					return (
-						<div className="buttonAppear">
-							<li className="item" key={i}>
-								{task}{" "}
-								<button value={i} onClick={deleteTodo}>
-									X
-								</button>
-							</li>
-						</div>
-					);
-				})}
-			</ul>
+			<div className="boxShadow">
+				<input
+					type="text"
+					id="first_name"
+					value={userInput}
+					onKeyDown={handleKeyPress}
+					onChange={event => {
+						setUserInput(event.target.value);
+					}}></input>
+				<ul>
+					{todolist.map((task, i) => {
+						return (
+							<div className="buttonAppear">
+								<li className="item" key={i}>
+									{task}{" "}
+									<button value={i} onClick={deleteTodo}>
+										X
+									</button>
+								</li>
+							</div>
+						);
+					})}
+					<li className="itemsLeft">items left</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
